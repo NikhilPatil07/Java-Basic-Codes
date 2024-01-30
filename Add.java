526e25a10ab13f38cfce676dd3221e632d1374d2
NikhilPatil07 <patilnikhil181804@gmail.com>
2024-01-30 23:11:12 +0530

    [ADDED]:Nikhil

---
+++ Add.java
@@ -0,0 +1,12 @@
+class Add
+{
+	public static void main(String args[])
+	{
+		int a,b,c,n=0;
+		a = Integer.parseInt(args[0]);
+		b = Integer.parseInt(args[1]);
+		c = Integer.parseInt(args[2]);
+		n = a/b/c;
+		System.out.println("Addition = "+n);
+	}
+}
